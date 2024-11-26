bb25a798257e25f9710afe08ce85c0def00b6727
YorkWilliam <49606461+YorkWilliam@users.noreply.github.com>
2024-11-26 01:11:30 -0600

    Updated Name and Description

--- scripts/background.js
+++ scripts/background.js
@@ -2,36 +2,36 @@
 
 // apply rules on folders
 messenger.menus.create({
-    id: "run-all-rules-on-folder",
-    title: "Apply All Rules",
+    id: "run-filters-on-folder",
+    title: "Apply Filters",
     contexts: ["folder_pane"]
 });
 
 messenger.menus.onClicked.addListener(async (info, tab) => {
-    if (info.menuItemId == "run-all-rules-on-folder") {
-        console.log("apply-all-rules-on-folder menu item clicked");
+    if (info.menuItemId == "run-filters-on-folder") {
+        console.log("run-filters-on-folder menu item clicked");
         try {
             browser.myapi.run_filters_on_folder();
-            console.log("apply-all-rules-on-folder finished");
+            console.log("run-filters-on-folder finished");
         } catch (error) {
-            console.log(`Error applying rules: ${error.message}`);
+            console.log(`Error applying filters: ${error.message}`);
         }
     }
 });
 
 // apply rules on messages
 messenger.menus.create({
-    id: "run-all-rules-on-message",
-    title: "Apply All Rules",
+    id: "run-filters-on-message",
+    title: "Apply Filters",
     contexts: ["message_list"]
 });
 
 messenger.menus.onClicked.addListener(async (info, tab) => {
-    if (info.menuItemId == "run-all-rules-on-message") {
-        console.log("apply-all-rules-on-message menu item clicked");
+    if (info.menuItemId == "run-filters-on-message") {
+        console.log("run-filters-on-message menu item clicked");
         try {
             browser.myapi.run_filters_on_message();
-            console.log("apply-all-rules-on-message finished");
+            console.log("run-filters-on-message finished");
         } catch (error) {
             console.log(`Error applying rules: ${error.message}`);
         }
